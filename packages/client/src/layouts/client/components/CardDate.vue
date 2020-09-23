d2a4b16396fd413112892c2092c74999c5476f83
Vadim <66322242+vadimqq@users.noreply.github.com>
2020-09-23 15:16:51 +0400

    add new component client CardDate

--- packages/client/src/layouts/client/components/CardDate.vue
+++ packages/client/src/layouts/client/components/CardDate.vue
@@ -1,14 +1,7 @@
 <template>
-  <div class="card-place">
-    <div class="card-place__photo">
-      <img :src="card.src">
-    </div>
-    <div class="card-place__info">
-      <span class="card-place__name">{{card.name}}</span>
-      <span class="card-place__rating">{{card.rating}}/10</span>
-      <span class="card-place__price">{{card.price}}руб.</span>
-      <a :href="card.link" target="_blank" class="card-place__link">(icon)</a>
-    </div>
+  <div class="card-date">
+    <span class="card-date__day">{{day}}</span>
+		<span class="card-date__month">{{month}}</span>
   </div>
 </template>
 
@@ -16,38 +9,41 @@
 import { Component, Prop, Vue } from 'vue-property-decorator'
 
 @Component
-export default class CardPlace extends Vue {
+export default class CardDate extends Vue {
 
 	@Prop({ default: ()=>{
 		return {}
 	}})
-  readonly card?: Object
-
+	readonly CardDate?: Object
+	
+data () {
+		return {
+				day: 21,
+				month: 'May'
+		}
+	}
 }
 </script>
 
-<style scoped>
-.card-place {
-  width: 200px;
-  height: 300px;
-  border: 1px solid gray;
-  border-radius: 10px;
-
-}
-.card-place__photo {
-  width: 100%;
-  height: 200px;
-  border-radius: 10px;
-  overflow: hidden;
-}
-.card-place__info {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+<style lang="scss" scoped>
+.card-date {
+	max-width: 40px;
+	max-height: 50px;
+	background-color: white;
+	border-radius: 10px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 10px;
+	&__day {
+		color: black;
+		font-size: 20px;
+		font-weight: 600;
+	}
+	&__month {
+		color: #ff5353;
+		font-size: 12px;
+		font-weight: 600;
+	}
 }
 </style>
